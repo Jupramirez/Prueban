f62ca7e151c8615826f04e2a3b89e0d0c4a27a2b
Juan Pablo Ramirez <jupramirezv@gmail.com>
2020-12-03 19:33:11 -0500

    otro ejercicios

---
+++ ejercicios/src/otro/ejercicios2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package otro;
+
+/**
+ *
+ * @author juanc
+ */
+public class ejercicios2 {
+    int entero = 5;
+
+    public int getEntero() {
+        return entero;
+    }
+
+    public void setEntero(int entero) {
+        this.entero = entero;
+    }
+    
+    public String carajo(){
+        return "Este es el numero final: "+ entero;
+    }
+    
+}
